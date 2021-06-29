03ade0fae87c80ebf346018ce596986b9920de51
Cedric H. David <cedric.david@jpl.nasa.gov>
2021-06-29 13:24:46 -0700

    Added new script to plot cdfs.

---
+++ src/rrr_anl_map_cdf.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+#*******************************************************************************
+#rrr_anl_map_cdf.py
+#*******************************************************************************
+
+#Purpose:
+#Given several CSV tables of spatial statistics related to RAPID simulations and
+#a folder name, this program creates a series of graphs with cumulative
+#distribution functions.
+#Author:
+#Cedric H. David, 2021-2021
+
+
+#*******************************************************************************
+#Import Python modules
+#*******************************************************************************
+import sys
+import os.path
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tkr
+
+
+#*******************************************************************************
+#Declaration of variables (given as command line arguments)
+#*******************************************************************************
+# 1 - rrr_map_csv
+# ...
+#n-1- rrr_map_csv
+# n - rrr_cdf_dir
+
+
+#*******************************************************************************
+#Get command line arguments
+#*******************************************************************************
+IS_arg=len(sys.argv)
+if IS_arg < 3:
+     print('ERROR - At least 2 arguments must be used')
+     raise SystemExit(22) 
+
+IS_map=IS_arg-2
+
+rrr_cdf_dir=sys.argv[IS_arg-1]
+rrr_cdf_dir=os.path.join(rrr_cdf_dir, '')
+#Add trailing slash to directory name if not present, do nothing otherwise
+
+
+#*******************************************************************************
+#Print input information
+#*******************************************************************************
+print('Command line inputs')
+for JS_map in range(IS_map):
+     print('- '+sys.argv[JS_map+1])
+print('- '+rrr_cdf_dir)
+
+
+#*******************************************************************************
+#Check if files exist 
+#*******************************************************************************
+for JS_map in range(IS_map):
+     rrr_map_csv=sys.argv[JS_map+1]
+     try:
+          with open(rrr_map_csv) as file:
+               pass
+     except IOError as e:
+          print('ERROR - Unable to open '+rrr_sts_csv)
+          raise SystemExit(22)
+
+if not os.path.isdir(rrr_cdf_dir):
+     os.mkdir(rrr_cdf_dir)
+
+
+#*******************************************************************************
+#Gathering headers from first file (assuming consistency across files)
+#*******************************************************************************
+print('Gathering headers from first file')
+rrr_map_csv=sys.argv[1]
+
+df=pandas.read_csv(rrr_map_csv)
+
+YS_map=df.columns[0]
+IV_riv=numpy.array(df[YS_map].tolist())
+IS_riv=len(IV_riv)
+
+YV_sts=list(df.columns)
+YV_sts=YV_sts[1:]
+IS_sts=len(YV_sts)
+
+print('- Number of river reaches: '+str(IS_riv))
+print('- Number of spatial statistic metrics: '+str(IS_sts))
+
+
+#*******************************************************************************
+#Plotting cumulative distribution functions
+#*******************************************************************************
+print('Plotting cumulative distribution functions')
+
+IV_pct=[i*1 for i in range(101)]
+#An array with percentage values going from 0 to 100 with increments of 5
+
+#-------------------------------------------------------------------------------
+#Plotting one spatial statistic over multiple experiments
+#-------------------------------------------------------------------------------
+print('- Plotting one spatial statistic over multiple experiments')
+
+for JS_sts in range(IS_sts):
+     YS_sts=YV_sts[JS_sts]
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     #Create curves
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     for JS_map in range(IS_map):
+          rrr_map_csv=sys.argv[JS_map+1]
+          df=pandas.read_csv(rrr_map_csv)
+          YS_map=df.columns[0]
+          ZV_sts=numpy.array(df[YS_sts].tolist())
+          if not numpy.isnan(ZV_sts).all():
+               ZV_cdf=numpy.nanpercentile(ZV_sts,IV_pct)
+               ZS_mea=int(round(numpy.nanmean(ZV_sts)))
+          else:
+               ZV_cdf=[0*i for i in IV_pct]
+          if JS_map==0:
+               plt.plot(ZV_cdf,IV_pct,color='k',linestyle='solid',linewidth=1, \
+                        label=YS_map+' ($\mu$='+str(ZS_mea)+')')
+          else:
+               plt.plot(ZV_cdf,IV_pct,linestyle='dotted',linewidth=0.5,        \
+                        label=YS_map+' ($\mu$='+str(ZS_mea)+')')
+
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     #Format figure
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     plt.ylim(0,100)
+     plt.axes().yaxis.set_major_locator(tkr.MultipleLocator(25))
+     plt.ylabel('Proportion (%)')
+
+     if YS_sts[0]=='Q':
+          plt.xlim(10**0.5,10**6)
+          plt.xlabel('Discharge ($m^3/s$)')
+          plt.xscale('log')
+
+     plt.legend(loc='lower right',prop={'family': 'monospace'})
+
+     YS_tit='Title'
+     if YS_sts=='Qout_avg': YS_tit='Spatial Distribution of Mean Discharge'
+     if YS_sts=='Qout_max': YS_tit='Spatial Distribution of Maximum Discharge'
+     if YS_sts=='Qout_min': YS_tit='Spatial Distribution of Minimum Discharge'
+     plt.title(YS_tit)
+
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     #Save and clear figure
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     plt.savefig(rrr_cdf_dir+YS_sts+'.pdf')
+     plt.clf()
+
+#-------------------------------------------------------------------------------
+#Plotting multiple spatial statistics over multiple experiments
+#-------------------------------------------------------------------------------
+print('- Plotting multiple spatial statistics over multiple experiments')
+
+if YV_sts[0][0]=='Q': YV_sts=['Qout_avg','Qout_max','Qout_min']
+if YV_sts[0][0]=='T': YV_sts=['Tevt_avg','Tevt_max','Tevt_min']
+
+for YS_sts in YV_sts:
+     YS_col='k'
+     if YS_sts=='Qout_avg' or YS_sts=='Tevt_avg': YS_col='b'
+     if YS_sts=='Qout_max' or YS_sts=='Tevt_max': YS_col='r'
+     if YS_sts=='Qout_min' or YS_sts=='Tevt_min': YS_col='g'
+
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     #Create curves
+     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+     for JS_map in range(IS_map):
+          rrr_map_csv=sys.argv[JS_map+1]
+          df=pandas.read_csv(rrr_map_csv)
+          YS_map=df.columns[0]
+          ZV_sts=numpy.array(df[YS_sts].tolist())
+          if not numpy.isnan(ZV_sts).all():
+               ZV_cdf=numpy.nanpercentile(ZV_sts,IV_pct)
+               ZS_mea=int(round(numpy.nanmean(ZV_sts)))
+          else:
+               ZV_cdf=[0*i for i in IV_pct]
+          if JS_map==0:
+               plt.plot(ZV_cdf,IV_pct,color=YS_col,linestyle='solid',          \
+                        linewidth=1.0,                                         \
+                        label=YS_map+' '+YS_sts+' ($\mu$='+str(ZS_mea)+')')
+          if JS_map==1:
+               plt.plot(ZV_cdf,IV_pct,color=YS_col,linestyle='dashed',         \
+                        linewidth=0.5,                                         \
+                        label=YS_map+' '+YS_sts+' ($\mu$='+str(ZS_mea)+')')
+          if JS_map==2:
+               plt.plot(ZV_cdf,IV_pct,color=YS_col,linestyle='dotted',         \
+                        linewidth=0.5,                                         \
+                        label=YS_map+' '+YS_sts+' ($\mu$='+str(ZS_mea)+')')
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#Format figure
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+plt.ylim(0,100)
+plt.axes().yaxis.set_major_locator(tkr.MultipleLocator(25))
+plt.ylabel('Proportion (%)')
+#
+if YS_sts[0]=='Q':
+     plt.xlim(10**0.5,10**6)
+     plt.xlabel('Discharge ($m^3/s$)')
+     plt.xscale('log')
+
+plt.legend(loc='lower right',prop={'family': 'monospace'})
+
+YS_tit=''
+plt.title(YS_tit)
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#Save and clear figure
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+plt.savefig(rrr_cdf_dir+YS_sts[0:5]+'cmb.pdf')
+plt.clf()
+
+
+#*******************************************************************************
+#End
+#*******************************************************************************
