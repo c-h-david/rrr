b5e9c0f776c960b2271416bf13ad0c524aace5fe
Cedric H. David <cedric.david@jpl.nasa.gov>
2022-08-17 10:42:15 -0700

    Added new unit tests.

---
+++ tst/tst_pub_dwnl_Collins_etal_202x_TBD.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#*******************************************************************************
+#tst_pub_dwnl_Collins_etal_202x_TBD.sh
+#*******************************************************************************
+
+#Purpose:
+#This script downloads all the files corresponding to:
+#Collins, E. L., 
+#DOI: xx.xxxx/xxxxxxxxxxxx
+#The files used are available from:
+#Collins, E. L., 
+#DOI: xx.xxxx/xxxxxxxxxxxx
+#The script returns the following exit codes
+# - 0  if all downloads are successful 
+# - 22 if there was a conversion problem
+# - 44 if one download is not successful
+#Author:
+#Cedric H. David, 2022-2022.
+
+
+#*******************************************************************************
+#Notes on tricks used here
+#*******************************************************************************
+#wget -q -nc           --> Quiet, No-clobber (don't overwrite) 
+#wget -r               --> Turn on recursive retrieving. 
+#wget -nH              --> Disable generation of host-prefixed directories. 
+#wget ---cut-dirs=i    --> Ignore i directory components when saving files. 
+#wget -P               --> Directory prefix where everything is downloaded
+
+
+#*******************************************************************************
+#Publication message
+#*******************************************************************************
+echo "********************"
+echo "Downloading files from:   https://doi.org/xx.xxxx/xxxxxxxxxxxx"
+echo "which correspond to   :   https://doi.org/xx.xxxx/xxxxxxxxxxxx"
+echo "These files are under a Creative Commons Attribution (CC BY) license."
+echo "Please cite these two DOIs if using these files for your publications."
+echo "********************"
+
+
+#*******************************************************************************
+#Download GLDAS2 monthly files
+#*******************************************************************************
+
+#-------------------------------------------------------------------------------
+#Download parameters
+#-------------------------------------------------------------------------------
+fld="../input/GLDAS"
+exp="GLDAS"
+frq="M"
+mod="                                                                          \
+     CLSM                                                                      \
+     NOAH                                                                      \
+     VIC                                                                       \
+    "
+str=(                                                                          \
+    "1980-01-01T00:00:00"                                                      \
+    "1981-01-01T00:00:00"                                                      \
+    )
+end=(                                                                          \
+    "1980-12-31T23:59:59"                                                      \
+    "1981-12-31T23:59:59"                                                      \
+    )
+
+#-------------------------------------------------------------------------------
+#Download process
+#-------------------------------------------------------------------------------
+mkdir -p $fld
+ndl=${#str[@]}
+#ndl is the number of download intervals
+
+for mod in $mod
+do
+for (( idl=0; idl<${ndl}; idl++ ));
+do
+     echo "Downloading GLDAS2 monthly data for $mod, from" ${str[$idl]} "to"   \
+           ${end[$idl]}
+     ../src/rrr_lsm_tot_ldas.py $exp $mod $frq ${str[$idl]} ${end[$idl]} $fld > tmp_dwl
+     if [ $? -gt 0 ] ; then echo "Problem downloading" && cat tmp_dwl >&2 ; exit 44 ; fi
+     rm tmp_dwl
+done
+done
+
+
+#*******************************************************************************
+#Download MERIT Hydro Basins input files
+#*******************************************************************************
+
+#-------------------------------------------------------------------------------
+#Download parameters
+#-------------------------------------------------------------------------------
+URL="http://hydrology.princeton.edu/data/mpan/MERIT_Basins/MERIT_Hydro_v07_Basins_v01/zip/pfaf_level_02/"
+folder="../input/MH07B01_TBD"
+list="                                                                         \
+      pfaf_74_MERIT_Hydro_v07_Basins_v01.zip	                               \
+     "
+
+#-------------------------------------------------------------------------------
+#Download process
+#-------------------------------------------------------------------------------
+mkdir -p $folder
+for file in $list
+do
+     wget -nv -nc $URL/$file -P $folder
+     if [ $? -gt 0 ] ; then echo "Problem downloading $file" >&2 ; exit 44 ; fi
+done
+
+
+#*******************************************************************************
+#Extract files
+#*******************************************************************************
+unzip -nq ../input/MH07B01_TBD/pfaf_74_MERIT_Hydro_v07_Basins_v01.zip -d ../input/MH07B01_TBD/
+if [ $? -gt 0 ] ; then echo "Problem converting" >&2 ; exit 22 ; fi
+
+
+#*******************************************************************************
+#Done
+#*******************************************************************************
---
+++ tst/tst_pub_repr_Collins_etal_202x_TBD.sh
@@ -0,0 +1,430 @@
+#!/bin/bash
+#*******************************************************************************
+#tst_pub_repr_Collins_etal_202x_TBD.sh
+#*******************************************************************************
+
+#Purpose:
+#This script reproduces all RRR pre- and post-processing steps used in the
+#writing of:
+#Collins, E. L., 
+#DOI: xx.xxxx/xxxxxxxxxxxx
+#The files used are available from:
+#Collins, E. L., 
+#DOI: xx.xxxx/xxxxxxxxxxxx
+#The following are the possible arguments:
+# - No argument: all unit tests are run
+# - One unique unit test number: this test is run
+# - Two unit test numbers: all tests between those (included) are run
+#The script returns the following exit codes
+# - 0  if all experiments are successful 
+# - 22 if some arguments are faulty 
+# - 33 if a search failed 
+# - 99 if a comparison failed 
+#Author:
+#Cedric H. David, 2020-2022
+
+
+#*******************************************************************************
+#Notes on tricks used here
+#*******************************************************************************
+#N/A
+
+
+#*******************************************************************************
+#Publication message
+#*******************************************************************************
+echo "********************"
+echo "Reproducing files for: https://doi.org/xx.xxxx/xxxxxxxxxxxx"
+echo "********************"
+
+
+#*******************************************************************************
+#Select which unit tests to perform based on inputs to this shell script
+#*******************************************************************************
+tot=99
+if [ "$#" = "0" ]; then
+     fst=1
+     lst=$tot
+     echo "Performing all unit tests: $1-$2"
+     echo "********************"
+fi 
+#Perform all unit tests if no options are given 
+
+if [ "$#" = "1" ]; then
+     fst=$1
+     lst=$1
+     echo "Performing one unit test: $1"
+     echo "********************"
+fi 
+#Perform one single unit test if one option is given 
+
+if [ "$#" = "2" ]; then
+     fst=$1
+     lst=$2
+     echo "Performing unit tests: $1-$2"
+     echo "********************"
+fi 
+#Perform all unit tests between first and second option given (both included) 
+
+if [ "$#" -gt "2" ]; then
+     echo "A maximum of two options can be used" 1>&2
+     exit 22
+fi 
+#Exit if more than two options are given 
+
+
+#*******************************************************************************
+#Initialize count for unit tests
+#*******************************************************************************
+unt=0
+
+
+#*******************************************************************************
+#River network details
+#*******************************************************************************
+
+#-------------------------------------------------------------------------------
+#Connectivity, base parameters, coordinates, sort
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Creating all domain files"
+../src/rrr_riv_tot_gen_all_meritbasins.py                                      \
+     ../input/MH07B01_TBD/riv_pfaf_74_MERIT_Hydro_v07_Basins_v01.shp           \
+     4                                                                         \
+     ../output/MH07B01_TBD/rapid_connect_pfaf_74_tst.csv                       \
+     ../output/MH07B01_TBD/kfac_pfaf_74_1km_hour_tst.csv                       \
+     ../output/MH07B01_TBD/xfac_pfaf_74_0.1_tst.csv                            \
+     ../output/MH07B01_TBD/sort_pfaf_74_topo_tst.csv                           \
+     ../output/MH07B01_TBD/coords_pfaf_74_tst.csv                              \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing connectivity"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/rapid_connect_pfaf_74.csv                           \
+     ../output/MH07B01_TBD/rapid_connect_pfaf_74_tst.csv                       \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+echo "- Comparing kfac"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/kfac_pfaf_74_1km_hour.csv                           \
+     ../output/MH07B01_TBD/kfac_pfaf_74_1km_hour_tst.csv                       \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+echo "- Comparing xfac"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/xfac_pfaf_74_0.1.csv                                \
+     ../output/MH07B01_TBD/xfac_pfaf_74_0.1_tst.csv                            \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+echo "- Comparing sorted IDs"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/sort_pfaf_74_topo.csv                               \
+     ../output/MH07B01_TBD/sort_pfaf_74_topo_tst.csv                           \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+echo "- Comparing coordinates"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/coords_pfaf_74.csv                                  \
+     ../output/MH07B01_TBD/coords_pfaf_74_tst.csv                              \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+rm -f $run_file
+rm -f $cmp_file
+echo "Success"
+echo "********************"
+fi
+
+#-------------------------------------------------------------------------------
+#Sorted subset
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Creating sorted basin file"
+../src/rrr_riv_bas_gen_one_meritbasins.py                                      \
+     ../input/MH07B01_TBD/riv_pfaf_74_MERIT_Hydro_v07_Basins_v01.shp           \
+     ../output/MH07B01_TBD/rapid_connect_pfaf_74.csv                           \
+     ../output/MH07B01_TBD/sort_pfaf_74_topo.csv                               \
+     ../output/MH07B01_TBD/riv_bas_id_pfaf_74_topo_tst.csv                     \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing sorted basin file"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/riv_bas_id_pfaf_74_topo.csv                         \
+     ../output/MH07B01_TBD/riv_bas_id_pfaf_74_topo_tst.csv                     \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+rm -f $run_file
+rm -f $cmp_file
+echo "Success"
+echo "********************"
+fi
+
+
+#*******************************************************************************
+#Contributing catchment information
+#*******************************************************************************
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Creating catchment file"
+../src/rrr_cat_tot_gen_one_meritbasins.py                                      \
+     ../input/MH07B01_TBD/cat_pfaf_74_MERIT_Hydro_v07_Basins_v01.shp           \
+     ../output/MH07B01_TBD/rapid_catchment_pfaf_74_tst.csv                     \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing catchment file"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/rapid_catchment_pfaf_74.csv                         \
+     ../output/MH07B01_TBD/rapid_catchment_pfaf_74_tst.csv                     \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+rm -f $run_file
+rm -f $cmp_file
+echo "Success"
+echo "********************"
+fi
+
+
+#*******************************************************************************
+#Process Land surface model (LSM) data - Monthly
+#*******************************************************************************
+
+#-------------------------------------------------------------------------------
+#Concatenating multiple files - Monthly - CLSM
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Concatenating multiple files - Monthly - CLSM"
+
+../src/rrr_lsm_tot_cmb_acc.sh                                                  \
+     ../input/GLDAS/GLDAS_CLSM10_M.2.0/1980/GLDAS_CLSM10_M.*.nc4               \
+     1                                                                         \
+     ../output/MH07B01_TBD/GLDAS_CLSM10_M_19800101_19801231_utc_tst.nc4        \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+#Using "1" because not averaging any consecutive files.
+
+echo "- Making concatenated file CF compliant - Monthly - CLSM"
+
+../src/rrr_lsm_tot_add_cfc.py                                                  \
+     ../output/MH07B01_TBD/GLDAS_CLSM10_M_19800101_19801231_utc_tst.nc4        \
+     1980-01-01T00:00:00                                                       \
+     2629800                                                                   \
+     2629800/10800                                                             \
+     ../output/MH07B01_TBD/GLDAS_CLSM10_M_19800101_19801231_utc_cfc_tst.nc4    \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+#Using "2629800" as the number of seconds in average month for 365.25 days/year.
+#Using "2629800/10800": monthly GLDAS files are time:mean of 3-hr accumulation.
+
+echo "- Comparing concatenated file CF compliant - Monthly - CLSM"
+
+./tst_cmp_n3d.py                                                               \
+     ../output/MH07B01_TBD/GLDAS_CLSM10_M_19800101_19801231_utc_cfc.nc4        \
+     ../output/MH07B01_TBD/GLDAS_CLSM10_M_19800101_19801231_utc_cfc_tst.nc4    \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+rm -f $run_file
+rm -f $cmp_file
+echo "Success"
+echo "********************"
+fi
+
+
+#*******************************************************************************
+#Coupling - Monthly
+#*******************************************************************************
+
+#-------------------------------------------------------------------------------
+#Create coupling file, CLSM
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Creating coupling file, CLSM"
+../src/rrr_cpl_riv_lsm_lnk.py                                                  \
+     ../output/MH07B01_TBD/rapid_connect_pfaf_74.csv                           \
+     ../output/MH07B01_TBD/rapid_catchment_pfaf_74.csv                         \
+     ../output/MH07B01_TBD/GLDAS_CLSM10_M_19800101_19801231_utc_cfc.nc4        \
+     ../output/MH07B01_TBD/rapid_coupling_pfaf_74_GLDAS_tst.csv                \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing coupling file"
+./tst_cmp_csv.py                                                               \
+     ../output/MH07B01_TBD/rapid_coupling_pfaf_74_GLDAS.csv                    \
+     ../output/MH07B01_TBD/rapid_coupling_pfaf_74_GLDAS_tst.csv                \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+rm -f $run_file
+rm -f $cmp_file
+echo "Success"
+echo "********************"
+fi
+
+#-------------------------------------------------------------------------------
+#Create volume file, CLSM
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Creating volume file, CLSM"
+../src/rrr_cpl_riv_lsm_vol.py                                                  \
+     ../output/MH07B01_TBD/rapid_connect_pfaf_74.csv                           \
+     ../output/MH07B01_TBD/coords_pfaf_74.csv                                  \
+     ../output/MH07B01_TBD/GLDAS_CLSM10_M_19800101_19801231_utc_cfc.nc4        \
+     ../output/MH07B01_TBD/rapid_coupling_pfaf_74_GLDAS.csv                    \
+     ../output/MH07B01_TBD/m3_riv_pfaf_74_19800101_19801231_CLSM10_M_utc_tst.nc4 \
+   > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing volume file, CLSM"
+./tst_cmp_ncf.py                                                               \
+     ../output/MH07B01_TBD/m3_riv_pfaf_74_19800101_19801231_CLSM10_M_utc.nc4   \
+     ../output/MH07B01_TBD/m3_riv_pfaf_74_19800101_19801231_CLSM10_M_utc_tst.nc4 \
+   > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+rm -f $run_file
+rm -f $cmp_file
+echo "Success"
+echo "********************"
+fi
+
+#-------------------------------------------------------------------------------
+#Update netCDF attributes, CLSM
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Updating netCDF attributes, CLSM"
+../src/rrr_cpl_riv_lsm_att.py                                                  \
+     ../output/MH07B01_TBD/m3_riv_pfaf_74_19800101_19801231_CLSM10_M_utc.nc4   \
+     'RRR data corresponding to MERIT Hydro 07 Basin 01 pfaf_74, GLDAS CLSM'   \
+     'Jet Propulsion Laboratory, California Institute of Technology'           \
+     ''                                                                        \
+     6378137                                                                   \
+     298.257222101                                                             \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing to NOTHING"
+
+rm -f $run_file
+echo "Success"
+echo "********************"
+fi
+
+
+#*******************************************************************************
+#Lumped matrix-based routing
+#*******************************************************************************
+
+#-------------------------------------------------------------------------------
+#Create lumped matrix-based routing, CLSM
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Creating lumped matrix-based routing, CLSM"
+../src/rrr_cpl_riv_lsm_rte.py                                                  \
+     ../output/MH07B01_TBD/m3_riv_pfaf_74_19800101_19801231_CLSM10_M_utc.nc4   \
+     ../output/MH07B01_TBD/rapid_connect_pfaf_74.csv                           \
+     ../output/MH07B01_TBD/riv_bas_id_pfaf_74_topo.csv                         \
+     ../output/MH07B01_TBD/Qout_pfaf_74_19800101_19801231_CLSM10_M_utc_tst.nc4 \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing lumped matrix-based routing, CLSM"
+./tst_cmp_ncf.py                                                               \
+     ../output/MH07B01_TBD/Qout_pfaf_74_19800101_19801231_CLSM10_M_utc.nc4     \
+     ../output/MH07B01_TBD/Qout_pfaf_74_19800101_19801231_CLSM10_M_utc_tst.nc4 \
+     > $cmp_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed comparison: $cmp_file" >&2 ; exit $x ; fi
+
+
+rm -f $run_file
+echo "Success"
+echo "********************"
+fi
+
+#-------------------------------------------------------------------------------
+#Update netCDF attributes, CLSM
+#-------------------------------------------------------------------------------
+unt=$((unt+1))
+if (("$unt" >= "$fst")) && (("$unt" <= "$lst")) ; then
+echo "Running unit test $unt/$tot"
+run_file=tmp_run_$unt.txt
+cmp_file=tmp_cmp_$unt.txt
+
+echo "- Updating netCDF attributes, CLSM"
+../src/rrr_cpl_riv_lsm_att.py                                                  \
+     ../output/MH07B01_TBD/Qout_pfaf_74_19800101_19801231_CLSM10_M_utc.nc4     \
+     'RRR data corresponding to MERIT Hydro 07 Basin 01 pfaf_74, GLDAS CLSM'   \
+     'Jet Propulsion Laboratory, California Institute of Technology'           \
+     ''                                                                        \
+     6378137                                                                   \
+     298.257222101                                                             \
+     > $run_file
+x=$? && if [ $x -gt 0 ] ; then echo "Failed run: $run_file" >&2 ; exit $x ; fi
+
+echo "- Comparing to NOTHING"
+
+rm -f $run_file
+echo "Success"
+echo "********************"
+fi
+
+
+#*******************************************************************************
+#Clean up
+#*******************************************************************************
+rm -f ../output/MH07B01_TBD/*_tst.*
+rm -f ../output/MH07B01_TBD/analysis/*_tst.*
+
+
+#*******************************************************************************
+#End
+#*******************************************************************************
+echo "Passed all tests!!!"
+echo "********************"
